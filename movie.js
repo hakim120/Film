5381b4678496e5d700770beb8c03fc8de94d301e
hizi <hizihakimsimplonenda@gmail.com>
2021-02-10 19:34:57 +0100

    Application Movie Api

---
+++ movie.js
@@ -0,0 +1,52 @@
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+console.log("Main balise",main)
+showMovies(apiUrl);
+function showMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    console.log(data.results);
+    data.results.forEach(element => {
+       
+     
+        const el = document.createElement('div');
+        const image = document.createElement('img');
+     
+        const divcardBody = document.createElement('div');
+        const cardText = document.createElement('p');
+        el.setAttribute("class","card");
+        image.setAttribute("class","card-img-top");
+        divcardBody.setAttribute("class","card-body");
+        cardText.setAttribute("class","card-text");
+        cardText.innerHTML = `${element.title}`;
+     
+        image.src = IMGPATH + element.poster_path;
+        divcardBody.appendChild(cardText);
+        el.appendChild(image);
+        el.appendChild(divcardBody);
+        main.appendChild(el);
+      
+   });
+    }); 
+};
+
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+     
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        showMovies(SEARCHAPI + searchTerm );
+  
+    }
+});
+
